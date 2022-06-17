066b5fa6341e67e737a784d6a34c9c7aa287ae4e
Kauan <kauan.maycon77@gmail.com>
2022-06-17 15:41:11 -0300

    Teste captura da imagem no array

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,8 @@ let imageBackCards = [`<img src="./img/bobrossparrot.gif" >`,`<img src="./img/ex
 ` <img src="./img/revertitparrot.gif">`,`<img src="./img/unicornparrot.gif"></img>`,`<img src="./img/bobrossparrot.gif" >`,`<img src="./img/explodyparrot.gif" >`,
 `<img src="./img/fiestaparrot.gif">`,` <img src="./img/tripletsparrot.gif">`,`  <img src="./img/metalparrot.gif">`,
 ` <img src="./img/revertitparrot.gif">`,`<img src="./img/unicornparrot.gif"></img>`];
-    
+
+
     
     let containerCards = document.querySelector(".containerCards");
     let amount = prompt('Coloque um número par de 4 a 14 para iniciar o jogo:');
@@ -12,13 +13,13 @@ let imageBackCards = [`<img src="./img/bobrossparrot.gif" >`,`<img src="./img/ex
     
     function numberChoice(){
         if(amount >= 4 && amount <= 14 && pair === 0 && amount!== null && amount !== isNaN(amount)){ 
-            for(let i = 0; i < amount; i++){ 
+            for(let i = 0; i < amount; i++){     
             containerCards.innerHTML += ` 
-                <ul class="card" >
+                <ul class="card " onclick="check()">
                     <li class="frontFace face " onclick="turnCard(this)">
                     <img src="./img/front.png" alt="parrot">
                     </li>
-                    <li class="backFace face" >
+                    <li class="backFace face " >
                     ${imageBackCards[i]}
                     </li>
                 </ul>`;
@@ -34,13 +35,27 @@ let imageBackCards = [`<img src="./img/bobrossparrot.gif" >`,`<img src="./img/ex
     function turnCard(card){
         let mainCard = card.parentNode;
         let haveFlip = mainCard.classList.contains("flip");
+        //let backCard = document.querySelector(".backFace");
         if (haveFlip) {
             mainCard.classList.remove("flip");
+            //backCard.classList.add("backFace");
         } else {
             mainCard.classList.add("flip");
+            //backCard.classList.remove("backFace");
           }
     }
 
 
-    
+    function check(){
    
+        for(let j = 0; j < imageBackCards.length; j++){
+            console.log(imageBackCards[j]);
+        }
+    }
+ /*
+    function capture(){
+        //captureBack = document.querySelector(".backFace .face img");
+        //captureBack = captureBack.innerHTML;
+        console.log("oi");
+    }
+   */ 
