b4d53a790255d54de26f08afa7f414648aec507d
Kauan <kauan.maycon77@gmail.com>
2022-06-16 21:28:44 -0300

    Correção do JS e adição de virar card

--- scripts/script.js
+++ scripts/script.js
@@ -1,130 +1,46 @@
-let cards = [
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/bobrossparrot.gif" >
-    </li>
-</ul>`,
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/explodyparrot.gif" >
-    </li>
-</ul>`, 
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/fiestaparrot.gif">
-    </li>
-</ul>`,
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/tripletsparrot.gif">
-    </li>
-</ul>`, 
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/metalparrot.gif">
-    </li>
-</ul>`, 
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/revertitparrot.gif">
-    </li>
-</ul>`,
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/bobrossparrot.gif" >
-    </li>
-</ul>`,
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="">
-    </li>
-    <li class="back-face face">
-    <img src="./img/unicornparrot.gif">
-    </li>
-</ul>`,
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/explodyparrot.gif" >
-    </li>
-</ul>`, 
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/fiestaparrot.gif">
-    </li>
-</ul>`,
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/tripletsparrot.gif">
-    </li>
-</ul>`, 
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/metalparrot.gif">
-    </li>
-</ul>`, 
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="parrot">
-    </li>
-    <li class="back-face face">
-    <img src="./img/revertitparrot.gif">
-    </li>
-</ul>`,
-`<ul class="card">
-    <li class="front-face face">
-    <img src="./img/front.png" alt="">
-    </li>
-    <li class="back-face face">
-    <img src="./img/unicornparrot.gif">
-    </li>
-</ul>` ];
 
-let containerCards = document.querySelector(".containerCards");
-let amount = prompt('Coloque um número par de 4 a 14 para iniciar o jogo:');
-let pair = amount%2;
+let imageBackCards = [`<img src="./img/bobrossparrot.gif" >`,`<img src="./img/explodyparrot.gif" >`,
+`<img src="./img/fiestaparrot.gif">`,` <img src="./img/tripletsparrot.gif">`,`  <img src="./img/metalparrot.gif">`,
+` <img src="./img/revertitparrot.gif">`,`<img src="./img/unicornparrot.gif"></img>`,`<img src="./img/bobrossparrot.gif" >`,`<img src="./img/explodyparrot.gif" >`,
+`<img src="./img/fiestaparrot.gif">`,` <img src="./img/tripletsparrot.gif">`,`  <img src="./img/metalparrot.gif">`,
+` <img src="./img/revertitparrot.gif">`,`<img src="./img/unicornparrot.gif"></img>`];
+    
+    
+    let containerCards = document.querySelector(".containerCards");
+    let amount = prompt('Coloque um número par de 4 a 14 para iniciar o jogo:');
+    let pair = amount%2;
+    
+    function numberChoice(){
+        if(amount >= 4 && amount <= 14 && pair === 0 && amount!== null && amount !== isNaN(amount)){ 
+            for(let i = 0; i < amount; i++){ 
+            containerCards.innerHTML += ` 
+                <ul class="card" >
+                    <li class="frontFace face " onclick="turnCard(this)">
+                    <img src="./img/front.png" alt="parrot">
+                    </li>
+                    <li class="backFace face" >
+                    ${imageBackCards[i]}
+                    </li>
+                </ul>`;
+            }
+        }else{
+            alert("Dados inválidos!");
+        }
+    }  
 
-function numberChoice(){
-    if(amount >= 4 && amount <= 14 && pair === 0 && amount!== null && amount !== isNaN(amount)){ 
-        for(let i = 0; i < amount; i++){ 
-        containerCards.innerHTML += `<div class="card">${cards[i]}</div>`;}
-    }else{
-        alert("Dados inválidos!");
+    numberChoice();
+
+
+    function turnCard(card){
+        let mainCard = card.parentNode;
+        let haveFlip = mainCard.classList.contains("flip");
+        if (haveFlip) {
+            mainCard.classList.remove("flip");
+        } else {
+            mainCard.classList.add("flip");
+          }
     }
-}  
-numberChoice();
 
 
-
+    
+   
